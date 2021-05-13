00ce0c2a1a17459dfa7a8fa770137b9fc8f7b98d
klucas72 <klucas72@hotmail.com>
2021-05-12 21:33:03 -0400

    finished all routes and models

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -5,13 +5,13 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_DATABASE, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
     user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     dialectOptions: {
       decimalNumbers: true,
     },
